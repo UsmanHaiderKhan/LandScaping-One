1f5f4cc3ed689ced87dd918ab6ba2f4cdc02d3f0
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-04-23 19:20:47 +0500

    Update the script file of the project added filtrizer script

--- LandScaping-One/assets/Bootstrap4/js/script.js
+++ LandScaping-One/assets/Bootstrap4/js/script.js
@@ -249,3 +249,45 @@ requestScrollAnimation((pos) => {
 
     }
 });
+
+/*===================== JS Gallery Filter ======================*/
+$(document).ready(function () {
+    $(".btn-act").on('click', function () {
+        $(".btn-act").removeClass("filter-active");
+        $(this).addClass("filter-active");
+    });
+});
+filterSelection("all")
+
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("no-display");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+}
+
+function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
